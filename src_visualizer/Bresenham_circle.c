cb5d0c7a07078f6e9884833445b8beecc7470398
Rajveer <rajveer14961496@gmail.com>
2025-11-05 18:00:43 +0530

    Basic Way of creating graphs, through ppm images

---
+++ src_visualizer/Bresenham_circle.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+#define height 1000
+#define width 1000
+
+int image[height][width];
+
+void plot(int centerX,int centerY,int x, int y)
+{
+    image[x+centerX][y+centerY] = 1;
+    image[y+centerX][x+centerY] = 1;
+    image[-x+centerX][-y+centerY] = 1;
+    image[-y+centerX][-x+centerY] = 1;
+    image[x+centerX][-y+centerY] = 1;
+    image[-y+centerX][x+centerY] = 1;
+    image[-x+centerX][y+centerY] = 1;
+    image[y+centerX][-x+centerY] = 1;
+}
+
+int main()
+{
+    int radius = 400;
+    int x=0;
+    int y=radius;
+    int d= 3- 2*radius;
+    int centerX = 500;
+    int centerY = 500;
+
+    plot(centerX,centerY,x,y);
+    for (;x<=y;x++)
+    {
+        if (d<0) d=d+4*x+6;
+        else y--,d=d+4*(x-y)+10;
+        plot(centerX,centerY,x,y);
+    }
+
+    FILE *f = fopen("output.ppm","w");
+
+    fprintf(f,"P3\n%d %d\n255\n",width,height);
+    for (int i=0;i<height;i++) //catches row
+    {
+        for (int j=0; j<width;j++) //catches column
+        {
+
+            if (image[i][j]==1) fprintf(f,"%d %d %d ",255,0,0); //our circle
+            else if (j == height/2 || i == width/2)fprintf(f, "%d %d %d ", 0, 0, 0); //graphing lines
+            else fprintf(f,"%d %d %d ",255,255,255); //white space
+        }
+        fprintf(f,"\n");
+    }
+    fclose(f);
+}
