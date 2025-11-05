3b393f8130c47b31b6096cc403aae49e79394d8c
Rajveer <rajveer14961496@gmail.com>
2025-11-05 20:20:20 +0530

    With basic visualizer

--- src_visualizer/Bresenham_circle.c
+++ src_visualizer/Bresenham_circle.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-#define height 1000
-#define width 1000
+#define height 300
+#define width 300
 
 int image[height][width];
 
@@ -17,21 +17,44 @@ void plot(int centerX,int centerY,int x, int y)
     image[y+centerX][-x+centerY] = 1;
 }
 
+void drawStep(int stepNo)
+{
+    char fileName[50];
+    sprintf(fileName,"steps/step_%d.ppm",stepNo);
+    FILE *f = fopen(fileName,"w");
+    fprintf(f,"P3\n%d %d\n255\n",width,height);
+    for (int i=0;i<height;i++) //catches row
+    {
+        for (int j=0; j<width;j++) //catches column
+        {
+
+            if (image[i][j]==1) fprintf(f,"%d %d %d ",255,0,0); //our circle
+            else if (j == height/2 || i == width/2)fprintf(f, "%d %d %d ", 0, 0, 0); //graphing lines
+            else fprintf(f,"%d %d %d ",255,255,255); //white space
+        }
+        fprintf(f,"\n");
+    }
+
+}
+
 int main()
 {
-    int radius = 400;
+    int radius = 50;
     int x=0;
     int y=radius;
     int d= 3- 2*radius;
-    int centerX = 500;
-    int centerY = 500;
+    int centerX = 150;
+    int centerY = 150;
+    int step =1;
 
     plot(centerX,centerY,x,y);
-    for (;x<=y;x++)
+    drawStep(step);
+    for (;x<=y;x++,step++)
     {
         if (d<0) d=d+4*x+6;
         else y--,d=d+4*(x-y)+10;
         plot(centerX,centerY,x,y);
+        drawStep(step);
     }
 
     FILE *f = fopen("output.ppm","w");
